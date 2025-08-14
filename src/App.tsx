f069f1dcaa6ba16da34006c1c8da17b5fdb470a0
pakosam <pako.s@yahoo.com>
2025-08-14 19:41:16 +0200

    main page created (showing players) and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { OptionIcon } from "./icons/OptionIcon";
+import { error } from "console";
+
+interface IPlayers {
+  id: number;
+  image?: string;
+  name: string;
+  surname: string;
+  club: string;
+}
 
 function App() {
+  const [players, setPlayers] = useState<IPlayers[]>();
+
+  useEffect(() => {
+    fetch("https://localhost:7066/api/Players")
+      .then((response) => response.json())
+      .then((data) => setPlayers(data))
+      .catch((error) => console.error("Error: ", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main-menu">
+      <div className="header-and-options-button">
+        <div>
+          <h1>Players Scouting</h1>
+        </div>
+        <div>
+          <OptionIcon />
+        </div>
+      </div>
+      <div className="selected-option">
+        <h3>Reports</h3>
+      </div>
+      <div className="list">
+        {players?.map((player, index) => {
+          const { image, name, surname, club } = player;
+          return (
+            <div key={`${index}${name}${surname}${club}`} className="player">
+              <div className="image-container">
+                <img
+                  className="player-image"
+                  src={image || "/assets/forward.png"}
+                />
+              </div>
+              <div className="player-info">
+                <p className="player-info-fullname">Scouting report: {name + " " + surname}</p>
+                <p className="player-info-club">Club: {club}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
