4b53ff2fd3784f62616d2e6c20b51aecc462ca18
pakosam <pako.s@yahoo.com>
2025-11-03 22:35:39 +0100

    axiosInstance created and now is user in authRepository

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,28 +1,24 @@
-import { ILogin, IRegister } from "../api/apiInterface";
+import { IRegister, ILogin } from "../api/apiInterface";
+import { axiosInstance } from "../api/axiosInstance";
 
-const url = "https://localhost:7066/api";
+class AuthRepository {
+  async register(credentials: IRegister) {
+    const response = await axiosInstance.post(
+      "/Authorization/register",
+      credentials
+    );
+    return response.data;
+  }
 
-export const authRepository = {
-  register: async (
-    registerData: Omit<IRegister, "id">
-  ): Promise<IRegister | undefined> => {
-    const response = await fetch(`${url}/Authorization/register`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(registerData),
-    });
-    if (!response.ok) throw new Error(`Failed to register: ${response.status}`);
+  async login(credentials: ILogin) {
+    const response = await axiosInstance.post(
+      "/Authorization/login",
+      credentials
+    );
+    localStorage.setItem("loginData", response.data);
 
-    return;
-  },
-  login: async (loginData: Omit<ILogin, "id">): Promise<ILogin | undefined> => {
-    const response = await fetch(`${url}/Authorization/login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(loginData),
-    });
-    if (!response.ok) throw new Error(`Failed to login: ${response.status}`);
+    return response.data;
+  }
+}
 
-    return;
-  },
-};
+export const authRepository = new AuthRepository();
